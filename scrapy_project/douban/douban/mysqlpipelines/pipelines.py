f4889b59df65aebe595211909aaa8141360715c8
rowlingz <ning_ML@126.com>
2019-04-01 22:41:05 +0800

    simple scrapy_test

---
+++ scrapy_project/douban/douban/mysqlpipelines/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+
+from douban.items import DoubanItem
+from .sql import Sql
+
+
+class DoubanPipeline(object):
+
+    def process_item(self, item, spider):
+        # pass
+        serial_number = item['serial_number']
+        evaluate = item['evaluate']
+        introduce = item['introduce']
+        movie_name = item['movie_name']
+        m_describe = item['m_describe']
+        star = item['star']
+
+        Sql.insert_dd_name(serial_number, movie_name, introduce, star, evaluate, m_describe)
+
+
+
---
+++ scrapy_project/douban/douban/mysqlpipelines/sql.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+
+import pymysql
+from douban import settings
+
+
+MYSQL_HOST = settings.MYSQL_HOST
+MYSQL_USER = settings.MYSQL_USER
+MYSQL_PASSWORD = settings.MYSQL_PASSWORD
+MYSQL_PORT = settings.MYSQL_PORT
+MYSQL_DB = settings.MYSQL_DB
+
+
+conn = pymysql.connect(host=MYSQL_HOST,
+                       port=MYSQL_PORT,
+                       user=MYSQL_USER,
+                       password=MYSQL_PASSWORD,
+                       database=MYSQL_DB,
+                       charset='utf8',)
+cursor = conn.cursor()
+
+
+class Sql:
+
+    @classmethod
+    def insert_dd_name(cls, serial_number, movie_name, introduce, star, evaluate, m_describe):
+        sql = "INSERT INTO top250movies(serial_number, movie_name, introduce, star, evaluate, m_describe) " \
+              "VALUES (%(serial_number)s, %(movie_name)s, %(introduce)s, %(star)s, %(evaluate)s, %(m_describe)s)"
+        value = {
+            'm_describe': m_describe,
+            'evaluate': evaluate,
+            'introduce': introduce,
+            'movie_name': movie_name,
+            'serial_number': serial_number,
+            'star': star,
+        }
+        cursor.execute(sql, value)
+        conn.commit()
