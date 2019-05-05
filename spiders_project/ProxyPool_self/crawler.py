b4dd96886df06c316af355aabbba0ce91961ebf7
rowlingz <ning_ML@126.com>
2019-05-05 17:45:01 +0800

    新增西刺免费代理的方法

--- spiders_project/ProxyPool_self/crawler.py
+++ spiders_project/ProxyPool_self/crawler.py
@@ -4,6 +4,9 @@
 """
 import requests
 from lxml import html
+from bs4 import BeautifulSoup
+import time
+import random
 
 
 class ProxyMetaclass(type):
@@ -20,12 +23,9 @@ class ProxyMetaclass(type):
 
 class Crawler(object, metaclass=ProxyMetaclass):
     def get_proxies(self, callback):
-        proxies = []
         for proxy in eval("self.{}()".format(callback)):
             print('成功获得到代理', proxy)
-            proxies.append(proxy)
-        print(len(proxies))
-        return proxies
+            yield proxy
 
     def crawl_shenji(self):
         shenji_url = "http://www.shenjidaili.com/open/"
@@ -43,3 +43,35 @@ class Crawler(object, metaclass=ProxyMetaclass):
         for ip in https_1:
             yield ip
 
+    def crawl_xici(self):
+        """新增西刺免费代理的获取"""
+        xici_url = "https://www.xicidaili.com/nn"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                          ' Chrome/73.0.3683.103 Safari/537.36'
+        }
+        response = requests.get(xici_url, headers=headers)
+
+        bf = BeautifulSoup(response.text, 'html.parser')
+        pages = bf.find('div', class_='pagination')
+        pages_total = pages.find_all('a')[-2].string
+        for i in range(1, 100):      # 爬取5500条时被封，每页包含100个，
+            url = "https://www.xicidaili.com/nn" + '/' + str(i)
+            bf = None
+            try:
+                response = requests.get(url, headers=headers)
+                if response.status_code == 200:
+                    bf = BeautifulSoup(response.text, 'html.parser')
+            except requests.RequestException:
+                continue
+            if bf:
+                ip_table = bf.find('table')
+                ip_list = ip_table.find_all('tr')
+                for ip in ip_list[1:]:
+                    ip_td = ip.find_all('td')
+                    ip_url = ip_td[1].string
+                    ip_port = ip_td[2].string
+                    ip_type = ip_td[5].string
+                    proxy = ':'.join([ip_url, ip_port])
+                    yield proxy
+                time.sleep(random.uniform(0.1, 3))
